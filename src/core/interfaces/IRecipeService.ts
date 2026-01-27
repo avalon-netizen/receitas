bc1433717859aa26c3674a7a2c9ccef3d597c688
Moises Bastos <moisebastosousa@gmail.com>
2026-01-27 15:20:43 -0300

    update: IRecipeService

--- src/core/interfaces/IRecipeService.ts
+++ src/core/interfaces/IRecipeService.ts
@@ -1,11 +1,14 @@
-import { Recipe, CreateRecipeInput } from "../models.js"
+import { Recipe, CreateRecipeInput, RecipeStatus } from "../models.js"
 
 export interface IRecipeService {
-  list(filter?: { categoryId?: string; categoryName?: string; search?: string }): Promise<Recipe[]>
+  list(filter?: { categoryId?: string; categoryName?: string; search?: string; status?: RecipeStatus | "all" }): Promise<Recipe[]>
   get(id: string): Promise<Recipe>
   create(input: CreateRecipeInput): Promise<Recipe>
   update(id: string, data: Partial<CreateRecipeInput>): Promise<Recipe>
   delete(id: string): Promise<void>
 
   scale(id: string, servings: number): Promise<Recipe>
+
+  publish(id: string): Promise<Recipe>
+  archive(id: string): Promise<Recipe>
 }
